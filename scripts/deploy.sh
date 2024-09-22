aeff4cbb6c22db6112243c0cd138490a6c754829
Benard Datascientist <139846345+Benard-Kimutai@users.noreply.github.com>
2024-09-22 03:05:34 +0300

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,12 @@
 # Deploys the microservices to Kubernetes.
 
 set -u
-: "$DATABASE_URL"
+
+# If DATABASE_URL is not set, assign a default value or error out
+DATABASE_URL="${DATABASE_URL:-'your-database-url-here'}"
+
+# Set your actual DATABASE_URL
+export DATABASE_URL='postgresql://part5_pjux_user:Yv9ZvKW7HyXWk2bIHP8FikyXVwH3wPvN@dpg-crniued6l47c73ah5nug-a.oregon-postgres.render.com/part5_pjux'
 
 # Apply the deployment and service configurations
 envsubst < ./kubernetes/book_catalog_deployment.yaml | kubectl apply -f -
