4f6544048f1718f4f09d33b7fbd131a77e9d19dc
Benard Datascientist <139846345+Benard-Kimutai@users.noreply.github.com>
2024-09-22 14:10:35 +0300

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,20 +2,14 @@
 
 # Deploys the microservices to Kubernetes.
 
-set -eu  # Exit on unset variables and errors
+set -u
+: "${DATABASE_URL}"
+: "${CONTAINER_REGISTRY}"
+: "${VERSION}"
 
-# Ensure DATABASE_URL is set
-: "${DATABASE_URL:?DATABASE_URL is not set}"
-
-
-echo "Using DATABASE_URL: $DATABASE_URL"
-
+# Export variables
+export DATABASE_URL CONTAINER_REGISTRY VERSION
 
 # Apply the deployment and service configurations
-echo "Substituting YAML for book_catalog_deployment.yaml"
-envsubst < ./kubernetes/book_catalog_deployment.yaml | tee /tmp/book_catalog_deployment.yaml | kubectl apply -f -
-
-echo "Substituting YAML for inventory_management_deployment.yaml"
-envsubst < ./kubernetes/inventory_management_deployment.yaml | tee /tmp/inventory_management_deployment.yaml | kubectl apply -f -
-
-echo "Deployment completed successfully."
+envsubst < ./kubernetes/book_catalog_deployment.yaml | kubectl apply -f -
+envsubst < ./kubernetes/inventory_management_deployment.yaml | kubectl apply -f -
