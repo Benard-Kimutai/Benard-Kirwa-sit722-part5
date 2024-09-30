bc472845e643756c956bad579cd2a0f57014b5be
Benard Datascientist <139846345+Benard-Kimutai@users.noreply.github.com>
2024-09-30 12:23:59 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,3 +12,22 @@ variable "kubernetes_version" {
   default = "1.30.1"
 }
 
+variable "azure_client_id" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "azure_client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+}
+
+variable "azure_tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "azure_subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
