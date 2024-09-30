cd04304b100d184372185935ee13239c3ac55620
Benard Datascientist <139846345+Benard-Kimutai@users.noreply.github.com>
2024-09-30 11:14:18 +0300

    Update terraform-apply.sh

--- scripts/terraform-apply.sh
+++ scripts/terraform-apply.sh
@@ -3,11 +3,13 @@
 # Navigate to the Terraform configuration directory
 cd terraform/
 
-# Apply the Terraform configuration with auto-approval
+# Apply the Terraform configuration with auto-approval and pass variables
 terraform apply -auto-approve \
   -var="version=${VERSION}" \
   -var="container_registry=${CONTAINER_REGISTRY}" \
   -var="registry_un=${REGISTRY_UN}" \
   -var="registry_pw=${REGISTRY_PW}" \
   -var="database_url=${DATABASE_URL}" \
-  -var="kube_config=${KUBE_CONFIG}"
+  -var="kube_config=${KUBE_CONFIG}" \
+  -var="app_name=${APP_NAME}" \  # Add this line
+  -var="app_version=${APP_VERSION}"  # Add this line
