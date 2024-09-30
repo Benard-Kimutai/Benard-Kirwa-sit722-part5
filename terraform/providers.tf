758d728e1e773a2608c2804f855969ae5791ab02
Benard Datascientist <139846345+Benard-Kimutai@users.noreply.github.com>
2024-09-30 12:23:12 +0300

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -11,5 +11,8 @@ terraform {
 provider "azurerm" {
   features {}
 
-  skip_provider_registration = true
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+  subscription_id  = var.azure_subscription_id
 }
