680a867b68b9e086b0a485351b3de98cf23c540a
Benard Datascientist <139846345+Benard-Kimutai@users.noreply.github.com>
2024-10-01 11:34:17 +0300

    Update terraform-apply.sh

--- scripts/terraform-apply.sh
+++ scripts/terraform-apply.sh
@@ -4,8 +4,4 @@
 cd terraform/
 
 # Apply the Terraform configuration with auto-approval and pass variables
-terraform apply -auto-approve \
-  -var="azure_client_id=${AZURE_CLIENT_ID}" \
-  -var="azure_client_secret=${AZURE_CLIENT_SECRET}" \
-  -var="azure_tenant_id=${AZURE_TENANT_ID}" \
-  -var="azure_subscription_id=${AZURE_SUBSCRIPTION_ID}"
+terraform apply -auto-approve
